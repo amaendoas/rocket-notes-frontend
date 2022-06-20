e294fbe6def5d0c37113cc251d33a77e2512136d
Amanda Guerra <guerraamanda1@hotmail.com>
2022-06-20 15:37:48 -0300

    Interface Home

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,14 @@ import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import GlobalStyles from './styles/global'
 
+import { Home } from './pages/Home'
 import { Details } from './pages/Details'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>
 )
