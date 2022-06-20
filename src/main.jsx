c4fa533d436398eddc209898da4a15cf603d4ded
Amanda Guerra <guerraamanda1@hotmail.com>
2022-06-20 16:27:26 -0300

    Interface Sign In e Sign Up

--- src/main.jsx
+++ src/main.jsx
@@ -6,12 +6,14 @@ import GlobalStyles from './styles/global'
 
 import { Home } from './pages/Home'
 import { Details } from './pages/Details'
+import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Home />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>
 )
--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,16 +1,22 @@
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
-import { FiMail, FiLock } from "react-icons/fi";
+import { FiUser, FiMail, FiLock } from "react-icons/fi";
 import { Container, Form, Background } from "./styles";
 
-export function SignIn() {
+export function SignUp() {
   return(
     <Container>
+      <Background/>
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
 
-        <h2>Faça seu login</h2>
+        <h2>Crie sua conta</h2>
+        <Input
+          placeholder="Nome"
+          type="text"
+          icon={FiUser}
+        />
         <Input
           placeholder="E-mail"
           type="text"
@@ -21,12 +27,9 @@ export function SignIn() {
           type="password"
           icon={FiLock}
         />
-        <Button title="Entrar">
-        </Button>
-
-        <a href="#">Criar Conta</a>
+        <Button title="Cadastrar" />
+        <a href="#">Voltar ao login</a>
       </Form>
-      <Background/>
     </Container>
   )
 }
